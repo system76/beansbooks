faf529551dc440050bf462f6798faa526cf3dd9a
David Overcash <funnylookinhat@gmail.com>
2014-12-31 14:37:49 -0700

    Fix a bad order_by statement.

--- application/classes/beans/setup/update/v/1/3/1.php
+++ application/classes/beans/setup/update/v/1/3/1.php
@@ -152,10 +152,8 @@ class Beans_Setup_Update_V_1_3_1 extends Beans_Setup_Update_V {
 		
 
 		$tax_payments = ORM::Factory('tax_payment')
-			->order_by(array(
-				'tax_id' => 'asc',
-				'date' => 'asc',
-			))
+			->order_by('tax_id','asc')
+			->order_by('date','asc')
 			->find_all();
 
 		// Not the most efficient loop, but it's easy to understand.
