5cc885c947a350fbefe19078452ac43eae14aae9
David Overcash <funnylookinhat@gmail.com>
2015-01-07 14:44:50 -0700

    Fix an issue with closing tabs - system76/beansbooks#203 - and added appropriate copyright header to all css and js files.

--- static/js/setup.js
+++ static/js/setup.js
@@ -1,3 +1,21 @@
+/*
+BeansBooks
+Copyright (C) System76, Inc.
+
+This file is part of BeansBooks.
+
+BeansBooks is free software; you can redistribute it and/or modify
+it under the terms of the BeansBooks Public License.
+
+BeansBooks is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+See the BeansBooks Public License for more details.
+
+You should have received a copy of the BeansBooks Public License
+along with BeansBooks; if not, email info@beansbooks.com.
+*/
+
 if ( document.body.className.match(new RegExp('(\\s|^)setup(\\s|$)')) !== null ) {
 
 	/**
