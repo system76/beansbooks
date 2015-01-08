27e499222e75e1028f1a77e5d1a65b78e1798bad
David Overcash <funnylookinhat@gmail.com>
2015-01-08 08:50:58 -0700

    Cascade 0.00 calibration logic to purchases.

--- application/classes/beans/vendor/purchase/calibrate/cancel.php
+++ application/classes/beans/vendor/purchase/calibrate/cancel.php
@@ -165,7 +165,8 @@ class Beans_Vendor_Purchase_Calibrate_Cancel extends Beans_Vendor_Purchase {
 
 		foreach( $account_transactions as $account_id => $amount ) 
 		{
-			if( $amount != 0.00 )
+			if( $purchase->total == 0.00 ||
+				$amount != 0.00 )
 			{
 				$account_transaction = new stdClass;
 				$account_transaction->account_id = $account_id;
